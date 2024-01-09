04383c965102346b81fd45277513a192dfb9db77
joepickin <joepickin@icloud.com>
2024-01-09 14:27:48 +0000

    more python

--- importing-a-csv.py
+++ importing-a-csv.py
@@ -18,8 +18,38 @@ airline_raw_df = pd.read_csv(
 
 # Explore the data
 
-# preview the first 5 rows
-print(airline_raw_df.head())
+# preview the first 10 rows
+print(airline_raw_df.head(10))
 
 # show the data types
-print(airline_raw_df.dtypes)
+print(airline_raw_df.dtypes)#
+
+# convert to the best possible data types
+airline_df = airline_raw_df.convert_dtypes()
+print(airline_df.dtypes)
+
+# Get info about the dataset
+print(airline_df.info())
+
+# Add a new Id column using the index +1 (the +1 moves the first number from 0 to 1)
+airline_df["Response Id"] = airline_df.index + 1
+print(airline_df["Response Id"].head(10))
+
+# Pivot from wide to long: this makes analyising survey data easier
+
+# Find the response columns (satisfaction, all columns scored from 0-5) that we want to pivot.
+response_columns = ['satisfaction', 'Seat comfort', 'Departure/Arrival time convenient', 'Food and drink', 'Gate location', 
+                       'Inflight wifi service', 'Inflight entertainment', 'Online support', 'Ease of Online booking', 
+                       'On-board service', 'Leg room service', 'Baggage handling', 'Checkin service', 'Cleanliness', 
+                       'Online boarding']
+
+# Everything else is info about the customer/ flight.
+categorical_columns = ['Response Id', 'Gender', 'Customer Type', 'Type of Travel', 'Class', "Age", 
+                       "Flight Distance", "Departure Delay in Minutes", "Arrival Delay in Minutes"]
+
+airline_long_df = pd.melt(airline_df, id_vars=categorical_columns, value_vars=response_columns)
+print(airline_long_df.head(20))
+
+# Sort by Response Id to make it look better
+airline_long_df = airline_long_df.sort_values(by=['Response Id'])
+print(airline_long_df.head(20))
